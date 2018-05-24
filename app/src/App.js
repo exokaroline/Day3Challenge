92c1658d805a8dfdeb8e314831ee027caa3d93e5
Caroline C <exokaroline@exokaroline.lan>
2018-05-24 10:45:30 -0400

    Succeses ;)

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  constructor(props) { 
+    super(props); 
+    this.state = {array: []}; 
   }
-}
+
+  componentDidMount( ) { 
+    axios.get("https://api.github.com/repos/facebook/react/stats/contributors")
+    .then (response => { 
+       const results = response.data;
+       var contributors = []; 
+       var i; 
+       for (i=0; i<results.length; i++) { 
+         var entry = {
+           username: results[i].author.login,
+           id: results[i].author.id, 
+           total: results[i].total
+         }
+        contributors.push(entry);
+       }
+       this.setState({
+          array: contributors
+       });
+    })
+  }
+  
+
+  render() {
+    const listy = (
+      <ul> {this.state.array.map((rest) =>
+        <li key={rest.id} > 
+          <h1> Username: {rest.username} </h1>
+          <h2> ID: {rest.id} </h2>
+          <h3> Commits: {rest.total} </h3>
+      </li>
+    )}
+      </ul>
+      );
+
+    return (
+        <div>{listy}</div>
+    );
+    }
+  }
+
+
+//ReactDOM.render(<App/>, document.getElementById('root'));
 
 export default App;
